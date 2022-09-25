6fa5ce460078737de570fd54cb9ed755eb10a30f
Sven Paulsen <info@sven-paulsen.de>
2022-09-25 21:24:44 +0200

    initial commit

---
+++ Source/meow/MeowGameModeBase.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeowGameModeBase.generated.h"
+
+UCLASS()
+class MEOW_API AMeowGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+};
